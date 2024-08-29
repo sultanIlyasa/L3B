dad2cce2615949227a232376f99437de1f627f71
Sultan Ilyasa <100148605+sultanIlyasa@users.noreply.github.com>
2024-08-29 19:44:38 +0700

    add-fonts

---
+++ src/app/fonts.ts
@@ -0,0 +1,12 @@
+import { Plus_Jakarta_Sans, Barlow_Condensed } from "next/font/google";
+
+export const plusJakartaSans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const barlowCondensed = Barlow_Condensed({
+  weight: ["400", "700"], // Add the weights you need
+  subsets: ["latin"],
+  display: "swap",
+});
